49489bb90db48cf19fcb219f609e560f4475fa60
xiongyanjun <872633801@qq.com>
2020-12-30 09:45:07 +0800

    first commit

---
+++ src/main/java/com/example/my_spring_cloud/MySpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_spring_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my_spring_cloud/MySpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_spring_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
